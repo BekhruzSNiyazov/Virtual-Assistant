ed9c31b012d16d3d42e2529190026140cb6bb2f6
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-01-03 22:28:01 +0200

    first commit

---
+++ data.py
@@ -0,0 +1,8 @@
+data = {
+    "exit": ["exit"],
+    "question_keywords": ["what", "why", "how"], 
+    "greeting": ["hi", "hey", "hello", "how are you", "how do you do", "whats up", "how are you doing", "howdy", "glad to see you"],
+    "self": ["im", "me", "i", "am"],
+    "good": ["good", "fine", "well", "wonderful", "perfect", "fantastic", "exceptional", "terrific", "outstanding", "great", "awesome"],
+    "bad": ["bad", "awful", "appailling", "dreadful", "terrible", "disgusting", "unwell", "uneasy"],
+}
