97328a17a2c13736951295166000adf76c16139f
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-01-03 23:48:47 +0200

    updated the data for more user-friendly answers

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
 data = {
     "exit": ["exit"],
-    "question_keywords": ["what", "why", "how"], 
-    "greeting": ["hi", "hey", "hello", "how are you", "how do you do", "whats up", "how are you doing", "howdy", "glad to see you"],
+    "question_keywords": ["What", "Why", "How"], 
+    "greeting": ["Hi!", "Hey", "Hello", "How are you?", "How do you do?", "What's up?", "How are you doing?", "Howdy", "Glad to see you"],
     "self": ["im", "me", "i", "am"],
     "good": ["good", "fine", "well", "wonderful", "perfect", "fantastic", "exceptional", "terrific", "outstanding", "great", "awesome"],
     "bad": ["bad", "awful", "appailling", "dreadful", "terrible", "disgusting", "unwell", "uneasy"],
