067e81898e7cf1c95a6ddae87b48688272447f1b
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-01-04 18:51:53 +0200

    updated data.py

--- data.py
+++ data.py
@@ -1,8 +1,8 @@
 data = {
     "exit": ["exit"],
-    "question_keywords": ["What", "Why", "How"], 
+    "question_keywords": ["What", "Why", "How", "Who"], 
     "greeting": ["Hi!", "Hey", "Hello", "How are you?", "How do you do?", "What's up?", "How are you doing?", "Howdy", "Glad to see you"],
-    "self": ["im", "me", "i", "am"],
+    "self": ["im", "i", "am", "my"],
     "good": ["good", "fine", "well", "wonderful", "perfect", "fantastic", "exceptional", "terrific", "outstanding", "great", "awesome"],
     "bad": ["bad", "awful", "appailling", "dreadful", "terrible", "disgusting", "unwell", "uneasy"],
 }
