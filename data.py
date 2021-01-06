c3ed544b48b03536db53cf801f5dabe106e35c3d
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-01-06 18:26:16 +0200

    updated data.py

--- data.py
+++ data.py
@@ -1,8 +1,10 @@
 data = {
-    "exit": ["exit"],
+    "exit": ["exit", "goodbye", "see you soon", "see you later", "bye", "cya", "see ya"],
     "question_keywords": ["What", "Why", "How", "Who"], 
-    "greeting": ["Hi!", "Hey", "Hello", "How are you?", "How do you do?", "What's up?", "How are you doing?", "Howdy", "Glad to see you"],
-    "self": ["im", "i", "am", "my"],
+    "greeting": ["Hi!", "Hey", "Hello", "How are you?", "How do you do?", "What's up?", "How are you doing?", "Howdy",\
+        "Glad to see you", "Good morning!", "Good evening!", "Good night!", "Good day!", "Sup", "Day", "Morning", "Evening", "Night", "Day", "Hulo", "Hullo"],
+    "themselves": ["im", "i", "am", "my"],
     "good": ["good", "fine", "well", "wonderful", "perfect", "fantastic", "exceptional", "terrific", "outstanding", "great", "awesome", "cool"],
-    "bad": ["bad", "awful", "appailling", "dreadful", "terrible", "disgusting", "unwell", "uneasy"],
+    "bad": ["bad", "awful", "appailling", "dreadful", "terrible", "disgusting", "unwell", "uneasy", "poor", "sad"],
+    "self": ["you", "youre"]
 }
