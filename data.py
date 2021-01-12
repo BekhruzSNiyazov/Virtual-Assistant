68e603d85b5f542510d12aa1770aa0944e00d6ec
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-01-12 12:11:09 +0200

    updated data.py

--- data.py
+++ data.py
@@ -1,9 +1,1 @@
-data = {
-    "exit": ["exit", "goodbye", "see you soon", "see you later", "bye", "cya", "see ya", "see you"],
-    "question_keywords": ["What", "Why", "How", "Who"], 
-    "greeting": ["Hi!", "Hey", "Hello", "How are you?", "How do you do?", "What's up?", "How are you doing?", "Howdy", "Glad to see you", "Good morning!", "Good evening!", "Good night!", "Good day!", "Good afternoon!", "Sup", "Day", "Morning","Evening", "Night",     "Afternoon", "Hulo", "Hullo", "Yo", "Hallo", "Halo"],
-    "themselves": ["im", "i", "am", "my"],
-    "good": ["good", "fine", "well", "wonderful", "perfect", "fantastic", "exceptional", "terrific", "outstanding", "great", "awesome", "cool", "excellent"],
-    "bad": ["bad", "awful", "appailling", "dreadful", "terrible", "disgusting", "unwell", "uneasy", "poor", "sad"],
-    "self": ["you", "youre"]
-}
+data = {'exit': ['exit', 'goodbye', 'see you soon', 'see you later', 'bye', 'cya', 'see ya', 'see you'], 'question_keywords': ['What', 'Why', 'How', 'Who'], 'greeting': ['Hi!', 'Hey', 'Hello', 'How are you?', 'How do you do?', "What's up?", 'How are you doing?', 'Howdy', 'Glad to see you', 'Good morning!', 'Good evening!', 'Good night!', 'Good day!', 'Good afternoon!', 'Sup', 'Day', 'Morning', 'Evening', 'Night', 'Afternoon', 'Hulo', 'Hullo', 'Yo', 'Hallo', 'Halo'], 'themselves': ['im', 'i', 'am', 'my'], 'good': ['good', 'fine', 'well', 'wonderful', 'perfect', 'fantastic', 'exceptional', 'terrific', 'outstanding', 'great', 'awesome', 'cool', 'excellent'], 'bad': ['bad', 'awful', 'appailling', 'dreadful', 'terrible', 'disgusting', 'unwell', 'uneasy', 'poor', 'sad', 'damn', 'dammit'], 'self': ['you', 'youre']}
